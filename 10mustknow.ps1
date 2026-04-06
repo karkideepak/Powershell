cb2fb2f28902899eb8991f8d7bc65a3e4c520ebd
Deepak <102180983+karkideepak@users.noreply.github.com>
2026-04-06 08:38:56 -0500

    Update 10mustknow.ps1

--- 10mustknow.ps1
+++ 10mustknow.ps1
@@ -1,1 +1,11 @@
 Get-Service
+Restart-Service
+Get-Process
+Stop-Process -Force
+Get-EvenLog -LogName System -Newest 50
+Get-LocalUser
+Set-ExecutionPolicy RemoteSigned
+Get-ComputerInfo
+Invoke-Command -Script
+Get-NetIpAddress
+# never run iex with irm with URL
