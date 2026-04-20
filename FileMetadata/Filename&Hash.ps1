b2e6948494cc73d15a487265e22f94ccf65da680
Deepak <102180983+karkideepak@users.noreply.github.com>
2026-04-20 16:24:17 -0500

    Update Filename&Hash.ps1

--- FileMetadata/Filename&Hash.ps1
+++ FileMetadata/Filename&Hash.ps1
@@ -4,4 +4,4 @@ Get-ChildItem -Recurse -File |
   Get-FileHash |
   Select-Object @{Name='Name';Expression={ Split-Path $_.Path -Leaf }}, Hash, Algorithm
 # Split-Path breaks a filesystem path into parts
-# -Leaf means “give me the last part only”
+# -Leaf means
